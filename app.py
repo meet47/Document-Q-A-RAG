0675653296ddc0ae7eea902117e49189dcbee25c
Meet Bardoliya <meetbardoliya28@gmail.com>
2024-06-11 17:24:35 -0400

    Version 1.3

--- app.py
+++ app.py
@@ -34,47 +34,50 @@ def read_pdf(file):
 
 #Streamlit App
 st.title("Meet's Document Q&A RAG App")
+
+if "pdf_text" not in st.session_state:
+    st.session_state.pdf_text = None
+
+if "vector_store" not in st.session_state:
+    st.session_state.vector_store = None
+
 pdf = st.file_uploader("Upload a PDF file", type="pdf")
+
+if pdf is not None and st.session_state.pdf_text is None:
+    st.session_state.pdf_text = read_pdf(pdf)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    splitted_text = text_splitter.split_documents(st.session_state.pdf_text)
+    st.session_state.vector_store = FAISS.from_documents(splitted_text, HuggingFaceBgeEmbeddings())
+
 user_input = st.text_input("Ask you question based on uploaded PDF")
 
-if pdf is not None:
-    pdf_text = read_pdf(pdf)
-    # st.text_area("PDF Content", pdf_text, height=500)
+if user_input and st.session_state.vector_store is not None:
 
-    #Split the text into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-    splitted_text = text_splitter.split_documents(pdf_text)
-
-    ##Setup VectorStore
-    vector_store = FAISS.from_documents(splitted_text, HuggingFaceBgeEmbeddings())
-
-    #Prompt
+    # Prompt
     prompt = ChatPromptTemplate.from_template("""
     Answer the following question based only on the provided context. 
     Think step by step before providing a detailed answer. 
     I will tip you $1000 if the user finds the answer helpful. 
-    Answer only if you find the answer. Otherwise say I couldn't find the answer
+    Answer only if you find the answer. Otherwise say I couldn't find the answer.
     <context>
     {context}
     </context>
     Question: {input}
                                         """)
 
-    #Chat Model Llama3-8b
+    # Chat Model Llama3-8b
     llm = ChatGroq(
-        groq_api_key = groq_api_key,
-        model_name = "llama3-8b-8192"
+        groq_api_key=groq_api_key,
+        model_name="llama3-8b-8192"
     )
 
-    document_chain = create_stuff_documents_chain(llm,prompt)
-    retriever = vector_store.as_retriever()
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vector_store.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    response = retrieval_chain.invoke({"input": user_input})
 
-    response = retrieval_chain.invoke({"input":user_input})
-
-    #Output for the User
-    if user_input:
-        st.write(response['answer'])
+    # Output for the User
+    st.write(response['answer'])
 
 
